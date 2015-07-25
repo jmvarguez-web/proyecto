25a3ba6ada63c566f7a59b620a0bf7975a9cedc3
jmvarguez-web <jmvarguez@hotmail.com>
2015-07-25 13:54:11 -0500

    v5

---
+++ v5/Automaya/Automaya-Bridging-Header.m
@@ -0,0 +1,10 @@
+//
+//  Automaya-Bridging-Header.m
+//  Automaya
+//
+//  Created by jmvarguez on 7/15/15.
+//  Copyright (c) 2015 jmvarguez. All rights reserved.
+//
+
+//#import <Foundation/Foundation.h>
+#import <GoogleMapsM4B/GoogleMaps.h>
